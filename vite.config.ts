32582c575093d70de380eb28e5d89eff53e2a214
YuhSylphy <yuhsylphy@gmail.com>
2026-02-20 18:13:30 +0900

    Configure base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const publish_name = "bookshelf";
+
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-})
+  base: `${mode === "production" ? publish_name : ""}/`,
+}));
