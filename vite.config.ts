158bbe155fd40164ca43d39294bfa9972894cfe2
YuhSylphy <yuhsylphy@gmail.com>
2026-02-21 21:12:51 +0900

    Configure to introduce absolute path references

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,28 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+import * as path from 'path';
+
 const publish_name = 'bookshelf';
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => ({
 	plugins: [react()],
 	base: `${mode === 'production' ? publish_name : ''}/`,
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+		},
+	},
 	test: {
 		environment: 'jsdom',
 		setupFiles: ['vitest.setup.ts'],
 		coverage: {
 			enabled: true,
 			include: ['src/**/*.{ts,tsx}'],
+		},
+		alias: {
+			'@': path.resolve(__dirname, './src'),
 		},
 	},
 }));
