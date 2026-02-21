af1bc2b0a51e5404dec49ebbec473bc09e894b28
YuhSylphy <yuhsylphy@gmail.com>
2026-02-21 15:55:16 +0900

    Configure test with vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-const publish_name = "bookshelf";
+const publish_name = 'bookshelf';
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => ({
-  plugins: [react()],
-  base: `${mode === "production" ? publish_name : ""}/`,
+	plugins: [react()],
+	base: `${mode === 'production' ? publish_name : ''}/`,
+	test: {
+		environment: 'jsdom',
+		setupFiles: ['vitest.setup.ts'],
+	},
 }));
